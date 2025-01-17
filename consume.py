a0f143ae51e66484778bf145689856ef97906d15
Kenny S <35939549+kxs6629@users.noreply.github.com>
2025-01-16 22:56:39 -0500

    More data manipulation to prep it for visualization

--- consume.py
+++ consume.py
@@ -2,22 +2,25 @@ def consume(filename):
     dataFile= open(filename, "r")
     
     day = 0
-    activityMap = {}
+    activityMap = []
     for line in dataFile:
         if line == "\n":
             day+=1
         else:
-            curLine = line.split(',')
+            cleanCut = line.rstrip()
+            curLine = cleanCut.split(',')
             dataLen = len(curLine)
             if dataLen == 1:
+                activityMap.append([day,curLine[0]])
                 print("generic activity")
             elif dataLen == 2:
+                activityMap.append([day,curLine[0],int(curLine[1])])
                 print("")
             elif dataLen == 3:
+                activityMap.append([day,curLine[0],curLine[1],curLine[2]])
                 print()
             else:
                 print("not sure how you got here man")
-            day+=1
 
     dataFile.close()
     return activityMap
--- display.py
+++ display.py
@@ -4,7 +4,8 @@ import matplotlib as plt
 # Right now I just want to take in the data from consume.py and display line graphs
 # in a window where you can change the data set on the fly (various activities)
 
-data = c.consume
+data = c.consume('data/work.txt')
+print(data)
 
 # In the future, this should have a prettier interface
 # Once I'm satisfeid with the desktop version, I can worry about making this mobile
