b3326cccd2a3d9d2cbaac10acddd49f226e4dadd
Kenny S <35939549+kxs6629@users.noreply.github.com>
2025-01-16 23:09:40 -0500

    More cleanup

--- consume.py
+++ consume.py
@@ -3,13 +3,17 @@ def consume(filename):
     
     day = 0
     activityMap = []
+    # parse data file
     for line in dataFile:
+        # if line is just a new line, that means new day
         if line == "\n":
             day+=1
         else:
             cleanCut = line.rstrip()
             curLine = cleanCut.split(',')
             dataLen = len(curLine)
+            # gets a little ugly here, but I need to sanitize it 
+            # To future me, we're working on sorting out how to store weight, time, etc. for the y value
             if dataLen == 1:
                 activityMap.append([day,curLine[0]])
                 print("generic activity")
