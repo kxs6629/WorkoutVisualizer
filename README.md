c5128113607f3e5f82ba4bba276b24f1c33fa401
Kenny S <35939549+kxs6629@users.noreply.github.com>
2025-01-16 20:05:16 -0500

    Initial commit and beginning implementation

---
+++ README.md
@@ -0,0 +1,9 @@
+# Workout Visualizer
+Homebrew data visualizer for my workouts over the last year. Ideally I want to build this out to continue to take in future data and display fresh graphs.
+
+## Core Functions
+* Consume data and visualize workout progress
+
+
+## Tech Stack (TBD)
+* Python
---
+++ consume.py
@@ -0,0 +1,22 @@
+def consume(filename):
+    dataFile= open(filename, "r")
+    
+    day = 0
+    activityMap = {}
+    for line in dataFile:
+        if line == "\n":
+            day+=1
+        else:
+            curLine = line.split(',')
+            dataLen = len(curLine)
+            if dataLen == 1:
+                print("generic activity")
+            elif dataLen == 2:
+                print("")
+            elif dataLen == 3:
+                print()
+            else:
+                print("not sure how you got here man")
+            day+=1
+
+    dataFile.close()
